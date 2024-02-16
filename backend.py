1d34b4a71ffe7a1c5f4311bf7b7c5894e1c911ff
alexgomezlopez <alexgomez1992@gmail.com>
2024-02-16 20:22:24 +0100

    añadido selector elegir colección

---
+++ backend.py
@@ -0,0 +1,41 @@
+# enviroment
+from dotenv import load_dotenv
+import os
+
+# eat pdfs
+from PyPDF2 import PdfReader
+
+# embeddings and llms
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+import openai
+
+# vector database
+from langchain.vectorstores import Qdrant
+import qdrant_client
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_vector_store():
+    # inicializar un cliente de Qdrant para interactuar con la base de datos vectorial
+    client = qdrant_client.QdrantClient(
+        os.getenv("QDRANT_HOST"),
+        api_key=os.getenv("QDRANT_API_KEY")
+    )
+    #utilizaremos este mecanismo de embedding
+    embeddings = OpenAIEmbeddings()
+    # creamos un objeto Qdrant, el cliente ya lo hemos definido y el método de embeddings también
+    vector_store = Qdrant(
+        client=client, 
+        collection_name=os.getenv("QDRANT_COLLECTION_NAME"), 
+        embeddings=embeddings,
+    )
+    
+    return vector_store
